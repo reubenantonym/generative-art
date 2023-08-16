a4211f80c1b01eccf51233097384b533bcbe6965
reubenantonym <retouchingr@gmail.com>
2023-08-16 15:58:15 +0300

    Created a function to create and draw the grid

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-const canvasSketch = require("canvas-sketch");
+import canvasSketch from "canvas-sketch";
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 1;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid();
+
   return ({ context, width, height }) => {
-    context.fillStyle = "orangered";
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 200, 0, Math.PI * 2, false);
+      context.strokeStyle = "blue";
+      context.lineWidth = 10;
+      context.stroke();
+    });
   };
 };
 
