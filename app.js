3e6ebde71661aa5a84d7a38440807b4c151e1c3b
reubenantonym <retouchingr@gmail.com>
2023-08-17 22:38:06 +0300

    added randomness on radius sizes using 'radius.gaussian()' method

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 40; ///Change this number for different design.
+    const count = 20; ///Change this number for different design.
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [u, v],
+        });
       }
     }
     return points;
@@ -29,24 +32,25 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+      const { position, radius } = data;
       // const x = u * width;
       // const y = v * height;
+      const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(
-        x,
-        y,
-        5 /*change this number to reduce the size of the circles*/,
-        0,
-        Math.PI * 2,
-        false
-      );
-      context.strokeStyle = "black";
-      context.lineWidth = 30; //Change this number to increase or decrease width size of your design.
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+
+      //// To fill circles with color
+      context.fillStyle = "orangered";
+      context.fill();
+
+      //// To create circles without fill
+      // context.strokeStyle = "black";
+      // context.lineWidth = 30; //Change this number to increase or decrease width size of your design.
+      // context.stroke();
     });
   };
 };
