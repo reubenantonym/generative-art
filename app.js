66a497caaaa6cd0920ef9f36a63714266dd30b58
reubenantonym <retouchingr@gmail.com>
2023-08-18 13:33:45 +0300

    Added noise property and removed/commented out seed randomness property

--- app.js
+++ app.js
@@ -17,16 +17,18 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 40; ///Change this number for different design.
+    const count = 30; ///Change this number for different design.
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
 
+        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(random.gaussian() * 0.01),
+          radius, //Math.abs(random.gaussian() * 0.01),
           position: [u, v],
         });
       }
@@ -34,7 +36,7 @@ const sketch = () => {
     return points;
   };
 
-  random.setSeed(615);
+  // random.setSeed(615);
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 120;
 
