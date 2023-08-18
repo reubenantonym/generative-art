53955ecedd926b352cafcadba0293dd798bacad3
reubenantonym <retouchingr@gmail.com>
2023-08-18 14:55:15 +0300

    Added another properties for drawing with text, word characters or emojis

--- app.js
+++ app.js
@@ -17,18 +17,19 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 30; ///Change this number for different design.
+    const count = 40; ///Change this number for different design.
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
 
-        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.25;
 
         points.push({
           color: random.pick(palette),
           radius, //Math.abs(random.gaussian() * 0.01),
+          rotation: random.noise2D(u, v),
           position: [u, v],
         });
       }
@@ -38,31 +39,48 @@ const sketch = () => {
 
   // random.setSeed(615);
   const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 120;
+
+  ///===== Canvas Margin ===///
+  const margin = 350;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
     points.forEach((data) => {
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation } = data;
       // const x = u * width;
       // const y = v * height;
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
+      /*
+      ////====== Creating circles properties =======//////
+    
       context.beginPath();
       context.arc(x, y, radius * width, 0, Math.PI * 2, false);
 
-      //// To fill circles with color
+      //==== To fill circles with color ====//
       context.fillStyle = color;
       context.fill();
 
-      //// To create circles without fill
+      //==== To create circles without fill ===///
       // context.strokeStyle = "black";
       // context.lineWidth = 30; //Change this number to increase or decrease width size of your design.
       // context.stroke();
+      */
+      ///====== drawing a letter (A,B,C,D ...) or any word characters ====///
+      context.save();
+      context.fillStyle = color;
+      context.font = `${radius * width}px "san-serif"`;
+
+      context.translate(x, y);
+      context.rotate(rotation);
+
+      //=== Use a string method for letters or words ===//
+      context.fillText("=", 0, 0);
+      context.restore();
     });
   };
 };
